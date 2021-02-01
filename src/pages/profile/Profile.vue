e5131ba7f989a679b686c6a902e7860428386adc
chenjiancong <1293707549@qq.com>
2021-02-01 09:57:36 +0800

    更新

--- src/pages/profile/Profile.vue
+++ src/pages/profile/Profile.vue
@@ -1,33 +1,33 @@
 <template>
   <div id="Profile">
-   <!-- <nav-bar>
+    <!-- <nav-bar>
      <div slot="center">我的中心</div>
-   </nav-bar> -->
-   <HeadLogin class="headLogin"></HeadLogin>
-   <GoodsOrder class="goodsOrder"></GoodsOrder>
- </div>
+    </nav-bar>-->
+    <HeadLogin class="headLogin"></HeadLogin>
+    <GoodsOrder class="goodsOrder"></GoodsOrder>
+  </div>
 </template>
 
 <script>
-import NavBar from "@/components/NavBar"
-import HeadLogin from "./children/HeadLogin"  //头部登录信息
-import GoodsOrder from "./children/GoodsOrder"  //商品订单信息
+import NavBar from "@/components/NavBar";
+import HeadLogin from "./children/HeadLogin"; ///头部登录信息
+import GoodsOrder from "./children/GoodsOrder"; //商品订单信息
 export default {
-   name:'Profile',
-   components:{NavBar,HeadLogin,GoodsOrder}
-}
+  name: "Profile",
+  components: { NavBar, HeadLogin, GoodsOrder }
+};
 </script>
 
 <style scoped>
-#Profile{
+#Profile {
   background-color: rgba(243, 243, 243, 0.945);
   height: 100vh;
 }
-.headLogin{
-background-color: rgb(235, 70, 59);
- height: 200px;
+.headLogin {
+  background-color: rgb(235, 70, 59);
+  height: 200px;
 }
-.goodsOrder{
-   background-color:white;
+.goodsOrder {
+  background-color: white;
 }
 </style>
